a378b1d1e0b6fed0367e8b7b3781afed178f2b94
April Kate Pascual <aprilkatepascual23@gmail.com>
2022-01-29 21:27:30 +0800

    modified

--- functions/pdf_generator/payslipgenerator.py
+++ functions/pdf_generator/payslipgenerator.py
@@ -33,31 +33,22 @@ month_year = 'January 2022'
 def create_payslip():
     for i in range (2, 42):
         #eading values from excel file
-        employee_name = sheet.cell(row = i, column = 1).value
-        address = sheet.cell(row = i, column = 2).value
-        reference = sheet.cell(row = i, column = 3).value
-        employer_name = sheet.cell(row = i, column = 4).value
-        email = sheet.cell(row = i, column = 5).value
-        job_status = sheet.cell(row = i, column = 6).value
-        postcode = sheet.cell(row = i, column = 7).value
-        grade = sheet.cell(row = i, column = 8).value
-        department = sheet.cell(row = i, column = 9).value
-        deminimis = sheet.cell(row = i, column = 10).value
-        basic_salary = sheet.cell(row = i, column = 11).value
-        overtime = sheet.cell(row = i, column = 12).value
-        gross_pay = sheet.cell(row = i, column = 13).value
-        net_pay = sheet.cell(row = i, column = 14).value
+        pay_date = sheet.cell(row = i, column = 1).value
+        employee_name = sheet.cell(row = i, column = 2).value
+        basic_salary = sheet.cell(row = i, column = 12).value
+        overtime = sheet.cell(row = i, column = 13).value
+        deminimis = sheet.cell(row = i, column = 11).value
+        gross_pay = sheet.cell(row = i, column = 14).value
+        
         tax = sheet.cell(row = i, column = 15).value
         sss = sheet.cell(row = i, column = 16).value
         loan = sheet.cell(row = i, column = 17).value
         philhealth_payment = sheet.cell(row = i, column = 18).value
         hdmf = sheet.cell(row = i, column = 19).value
         deductions = sheet.cell(row = i, column = 20).value
-        pay_date = sheet.cell(row = i, column = 21).value
-        philhealth_number = sheet.cell(row = i, column = 22).value
-        taxable_pay = sheet.cell(row = i, column = 23).value
-        pension_pay = sheet.cell(row = i, column = 24).value
-        other_payment_due = sheet.cell(row = i, column = 25).value
+        
+        net_pay = sheet.cell(row = i, column = 21).value
+        
 
         #Creating a pdf file and setting a naming convention
         c = canvas.Canvas('functions/pdf_generator/data/'+str(employee_name)+'_'+ month_year + '.pdf' )
@@ -76,48 +67,20 @@ def create_payslip():
         y = 2500
         #Drawing payslip related information
         c.setFont('Ayar',45)
+        c.drawString(start, y, 'Pay Date - dd/mm/yy :')
+        c.drawString(start_2, y, str(pay_date))
+        y -= spread
+        
         c.drawString(start, y, 'Employee\'s name:')
         c.drawString(start_2, y, str(employee_name) + ' ')
         y -= spread
 
-        c.drawString(start, y, 'Address:')
-        c.drawString(start_2, y, str(address))
-        y -= spread
-
-        c.drawString(start, y, 'Reference:')
-        c.drawString(start_2, y, str(reference))
-        y -= spread
-
-        c.drawString(start, y, 'Employer Name:')
-        c.drawString(start_2, y, str(employer_name))
-        y -= spread
-
-        c.drawString(start, y, 'Email:')
-        c.drawString(start_2, y, str(email))
-        y -= spread
-        
-        c.drawString(start, y, 'Job Status:')
-        c.drawString(start_2, y, str(job_status))
-        y -= spread
-
-        c.drawString(start, y, 'Post Code:')
-        c.drawString(start_2, y, str(postcode))
-        y -= spread
-
-        c.drawString(start, y, 'Grade:')
-        c.drawString(start_2, y, str(grade))
-        y -= spread
-
-        c.drawString(start, y, 'Department:')
-        c.drawString(start_2, y, str(department))
-        y -= spread
-
-        c.drawString(start, y, 'De Minimis:')
-        c.drawString(start_2, y, str(deminimis))
-        y -= spread
-
         c.drawString(start, y, 'Basic Salary:')
         c.drawString(start_2, y, str(basic_salary))
+        y -= spread
+        
+        c.drawString(start, y, 'De Minimis:')
+        c.drawString(start_2, y, str(deminimis))
         y -= spread
 
         c.drawString(start, y, 'Overtime:')
@@ -126,10 +89,6 @@ def create_payslip():
 
         c.drawString(start, y, 'Gross Pay:')
         c.drawString(start_2, y, str(gross_pay))
-        y -= spread
-
-        c.drawString(start, y, 'Net Pay:')
-        c.drawString(start_2, y, str(net_pay))
         y -= spread
 
         c.drawString(start, y, 'Tax:')
@@ -156,29 +115,9 @@ def create_payslip():
         c.drawString(start_2, y, str(deductions))
         y -= spread
 
-        c.drawString(start, y, 'Pay Date - dd/mm/yy :')
-        c.drawString(start_2, y, str(pay_date))
-        y -= spread
-
-        c.drawString(start, y, 'PhilHealth Number:')
-        c.drawString(start_2, y, str(philhealth_number))
-        y -= spread
-
-        c.drawString(start, y, 'Taxable Pay:')
-        c.drawString(start_2, y, str(taxable_pay))
-        y -= spread
-
-        c.drawString(start, y, 'SSS Pay:')
-        c.drawString(start_2, y, str(pension_pay))
-        y -= spread
-
-        c.drawString(start, y, 'Other Payment Due:')
-        c.drawString(start_2, y, str(other_payment_due))
-        y -= spread
-
         c.drawString(start, y, 'Net Pay:')
         c.drawString(start_2, y, str(net_pay))
-        y -= spread * 3
+        y -= spread
 
         c.drawString(start, y, 'Signature: ')
         c.drawString(start_2, y,'____________')
